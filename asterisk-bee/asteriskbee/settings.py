b09acf6ffc73371b6d317583a10a3e0310e1a589
emoron <emoronayuso@gmail.com>
2014-04-10 09:24:35 +0200

    Se modifica el fichero settings.py, solucionando un error de ruta del directorio raiz

--- asterisk-bee/asteriskbee/settings.py
+++ asterisk-bee/asteriskbee/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/asterisk-bee/asteriskbee/'
+STATIC_ROOT = '/var/www/asterisk-bee/asteriskbee/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
