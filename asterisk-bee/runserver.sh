be77b08832d9b47e3556a775d8b6694a47eaa21a
emoron <emoronayuso@gmail.com>
2014-04-11 14:53:21 +0200

    Se crea script para iniciar Beeton

---
+++ asterisk-bee/runserver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] || [ $# -gt 1 ]; then
+   echo "USO:"
+   echo "Arrancar Beeton: ./runserver.sh start" 
+   echo "Detener Beeton: ./runserver.sh stop"
+else
+   if [ $1 == "start" ]; then
+     python manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
+     echo "Beeton se ha iniciado" 
+   elif [ $1 == "stop" ]; then
+     pid=`netstat -lpn | grep 8000 | tr -s ' ' '-' | cut -d '-' -f7 | cut -d '/' -f1` 
+     kill -9 $pid
+     echo "Beeton se ha parado"
+   else
+     echo "Opcion incorrecta"
+   fi
+fi
